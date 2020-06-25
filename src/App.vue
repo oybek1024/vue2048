f91dd9fba6fed2e93a162bf6a2e2a43da2e0b4f2
oybek1024 <mukhiddinov.oybek1024@gmail.com>
2020-06-25 09:40:06 +0500

    2048 Game

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="title">Vue 2048 Game</p>
+    <MainContent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainContent from './components/MainContent'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MainContent
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  }  
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+
+    .title {
+        padding: 15px;
+        text-align: center;
+        font-size: 1.5rem;
+    }
 }
 </style>
---
+++ src/components/TotalCounter.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="actions">
+        <div class="stats">
+            <div class="score">Score: {{ info.score }}</div>
+        </div>
+        <div class="buttons">
+            <button class="btn" v-if="info.gameState !== 'RUNNING'" @click="startGame">Start Game</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        info: { numMoves: Number, highest: Number, score: Number, gameState: String, timeDiff: Number },
+    },
+    methods: {
+        startGame() {
+            this.$emit("startGame");
+        },
+    },
+};
+</script>
+<style scoped lang="scss">
+
+.actions {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    .stats {
+        --font-size: 1.2em;
+
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+
+        .score {
+            padding: 10px;
+            font-size: var(--font-size);
+            font-weight: bold;
+            line-height: 2em;
+        }
+    }
+
+    .buttons {
+        display: flex;
+        justify-content: flex-end;
+        margin: 10px 0;
+    }
+
+    .btn {
+        color: white;
+        background-color: yellowgreen;
+        padding: 10px 30px;
+        font-size: 12px;
+        
+    }
+}
+
+@media screen and (min-width: 355px) and (max-width: 499px) {
+    .actions .stats {
+        --font-size: 1em;
+    }
+}
+</style>
